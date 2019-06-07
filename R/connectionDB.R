02cc4985fc5b467c490dcefa81203f95f613446e
neal1202 <neal1202@ajou.ac.kr>
2019-06-07 10:38:50 +0900

    added NAMESPACE

--- R/connectionDB.R
+++ R/connectionDB.R
@@ -1,4 +1,12 @@
-ohdsidatacatalogue <- function (Server,IP,Schema,ID,PW)
+#' OhdsiDataCatalogue Function
+#'
+#' This function allows you to see basic elements of the data catalogue automatically.
+#' @param Server,IP,Schema,ID,PW : connection details.
+#' @keywords data catalogue
+#' @export
+#' @examples
+#' OhdsiDataCatalogue("sql server","IP","cdmdatabaseSchema","ID","PW")
+OhdsiDataCatalogue <- function (Server,IP,Schema,ID,PW)
 {
   connectionDetails <- DatabaseConnector::createConnectionDetails(dbms=Server,
                                                                   server=IP,
